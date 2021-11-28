d7d3cd13c760022ebfd6a19868067069112591e8
nabilfsd <nabil.the.fsd@gmail.com>
2021-11-28 14:13:50 +0600

    feat: complete upto main header

---
+++ main.js
@@ -0,0 +1,19 @@
+// css
+// bootstrap css file
+import './src/css/plugins/bootstrap.min.css'
+
+// custom css files
+import './src/css/custom.css';
+
+// custom scss file
+import './src/scss/main.scss'
+
+// js
+// bootstrap js files
+import './src/js/plugins/popper.min'
+import './src/js/plugins/bootstrap.min';
+
+// custom script files
+import './src/js/custom'
+
+
